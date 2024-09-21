8029d01a51d9bc643c450ce94d3d6e8601ca5fbb
Anders35 <andersleo05@gmail.com>
2024-09-21 16:05:28 +0700

    tugas 4

--- shield/settings.py
+++ shield/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1lcvjqa!9heziyr!88#e3%3__4^!aza7w7c(aoz+8ey9ww$dd$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "anders-willard-shield.pbp.cs.ui.ac.id"]
 
